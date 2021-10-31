6a5f226c6c0257f152e8330c3ac0b3e8ecbda8b8
lazymartin <gzljt@163.com>
2021-10-31 09:53:21 +0800

    push

--- install_xray.sh
+++ install_xray.sh
@@ -1,32 +1,64 @@
 #!/bin/bash
-#字体颜色
+# font color
 Green="\033[32m"
 Red="\033[31m"
 Yellow="\033[33m"
 Font="\033[0m"
 
-#提示信息
+# message type
 OK="${Green}[OK] "
 Error="${Red}[Error] "
 Yellow="${Yellow}[WARNING] "
 
-rm -rf xray geo*.dat
+if [[ -z ${webdav_link} ]]; then
+  download_link="https://github.com/lazymartin/scripts/releases/download/Last_release/"
+  config_link="https://raw.githubusercontent.com/lazymartin/scripts/master/configurations/xray/"
+  else
+  download_link="--http-user=${webdav_username} --http-passwd=${webdav_password} ${webdav_link}"
+  config_link="--http-user=${webdav_username} --http-passwd=${webdav_password} ${webdav_link}"
+fi
 
-#检查是否root用户
+if [[ -f /usr/local/bin/xray ]]; then
+  install_type="UPGRADED"
+  else
+  install_type="INSTALLED"
+fi
+
+#Check if root user
 [ `id -u` != 0 ] && echo && echo -e "${Error}The current user isn't \"root\", please switch to the root user and execute the script again!${Font}" && echo && exit 1
+
+system_type=`cat /etc/issue|head -n 1|awk '{print $1}'` # get system type
+arch_type=`uname -m` # get arch type
+
+if [[ ${arch_type} = "x86_64" ]]; then
+  xray_file="xray_linux_x86_64"
+elif [[ ${arch_type} = "aarch64" ]]; then
+  xray_file="xray_linux_arm64_v8a"
+else
+  echo && echo -e "${Error}Only supports x86_64 or aarch64 architecture!!${Font}" && echo && exit 1
+fi
+
+if [[ "${system_type}" = "debian" || "${system_type}" = "ubuntu" || "${system_type}" = "Armbian" ]]; then
+  echo ""
+  else
+  echo && echo -e "${Error}Only supports Debian or Ubuntu system!!${Font}" && echo && exit 1
+fi
+
+
+rm -rf xray geo*.dat
 
 [ "$(dpkg --list|grep wget)" == "" ] && echo -e "${OK}Installing wget, please wait...${Font}" && apt update &>/dev/null && apt install -y wget &>/dev/null
 
 echo -e "${OK}Downloading xray, please wait...${Font}"
 #curl -O --progress-bar $LINK
-wget -q --show-progress https://github.com/lazymartin/scripts/releases/download/Last_release/xray_linux-x64 -O xray
-[[ $? -ne 0 ]] && echo -e "${Error}Download xray failed, please check the network!${Font}" && exit
+wget -q --show-progress ${download_link}${xray_file} -O xray
+[[ $? -ne 0 ]] && echo -e "${Error}Download xray failed, please check network connection!${Font}" && exit
 
-wget -q --show-progress https://github.com/lazymartin/scripts/releases/download/Last_release/geoip.dat
-[[ $? -ne 0 ]] && echo -e "${Error}Download geoip.dat failed, please check the network!${Font}" && exit
+wget -q --show-progress ${download_link}geoip.dat
+[[ $? -ne 0 ]] && echo -e "${Error}Download geoip.dat failed, please check network connection!${Font}" && exit
 
-wget -q --show-progress https://github.com/lazymartin/scripts/releases/download/Last_release/geosite.dat
-[[ $? -ne 0 ]] && echo -e "${Error}Download geosite.dat failed, please check the network!${Font}" && exit
+wget -q --show-progress ${download_link}geosite.dat
+[[ $? -ne 0 ]] && echo -e "${Error}Download geosite.dat failed, please check network connection!${Font}" && exit
 
 systemctl stop xray &>/dev/null
 
@@ -49,10 +81,9 @@ if [[ ! -f /usr/local/etc/xray/config.json ]]; then
   https://github.com/XTLS/Xray-examples${Font}"
 fi
 
-[[ ! -f /usr/lib/systemd/system/xray.service ]] && wget -q --show-progress https://raw.githubusercontent.com/lazymartin/scripts/master/configurations/xray/xray.service -O /usr/lib/systemd/system/xray.service 
+[[ ! -f /usr/lib/systemd/system/xray.service ]] && wget -q --show-progress ${config_link}xray.service -O /usr/lib/systemd/system/xray.service 
 
 active_stat=`systemctl list-units --type=service --state=active |grep xray`
 [[ -z ${active_stat} ]] && systemctl enable xray &>/dev/null
 
-echo -e "${OK}The xray with latest commit has been installed!${Font}"
-
+echo -e "${OK}The xray with latest commit has been ${install_type}!${Font}"
